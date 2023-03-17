98f541eb0d7925a46e163452c75a945bf8510e5e
dhsong215 <77286910+dhsongdev@users.noreply.github.com>
2023-03-17 15:10:37 +0900

    create account

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 
 //components
 import InNav from './navigator/InNav';
@@ -7,6 +8,16 @@ import OutNav from './navigator/OutNav';
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    console.log(user);
+    const subscriber = auth().onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    if (user) setLoggedIn(true);
+    return subscriber;
+  }, [user]);
 
   return (
     <NavigationContainer>
--- screens/Join.js
+++ screens/Join.js
@@ -1,20 +1,54 @@
 import React, { useCallback, useRef, useState } from 'react';
-import {
-  StyleSheet,
-  View,
-  TextInput,
-  Button,
-  TouchableOpacity,
-} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import { StyleSheet, View, TextInput, Button, Text, Alert } from 'react-native';
 
 export default function Join({ navigation: { goBack } }) {
   const passwordRef = useRef();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [authLoading, setAuthLoading] = useState(false);
 
   const onSubmitEditingEmail = useCallback(() => {
     passwordRef.current.focus();
   }, []);
+
+  const onSubmitEditingPassword = () => {
+    setAuthLoading(true);
+    if (password === '') {
+      Alert.alert('Password', 'Fill the password');
+      setAuthLoading(false);
+      return;
+    }
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error.code);
+        console.log(password);
+        if (error.code === 'auth/email-already-in-use') {
+          Alert.alert(
+            'Check your email, please.',
+            'That email address is already in use!'
+          );
+          return;
+        }
+        if (error.code === 'auth/invalid-email') {
+          Alert.alert(
+            'Check your email, please.',
+            'That email address is invalid!'
+          );
+          return;
+        }
+        if (error.code === 'auth/weak-password') {
+          Alert.alert(
+            'Check your password, please.',
+            'Use stronger password by using 특수기호 or numbers'
+          );
+          return;
+        }
+        setAuthLoading(false);
+        goBack();
+      });
+  };
 
   return (
     <View style={{ alignItems: 'center' }}>
@@ -38,17 +72,22 @@ export default function Join({ navigation: { goBack } }) {
           setPassword(e);
         }}
         secureTextEntry
-        textContentType="password"
         returnKeyType="done"
         placeholder="password"
         value={password}
       />
-      <Button
-        title="join"
-        onPress={() => {
-          goBack();
-        }}
-      />
+      <View>
+        {authLoading ? (
+          <Text>Creating account...</Text>
+        ) : (
+          <Button
+            title="join"
+            onPress={() => {
+              onSubmitEditingPassword();
+            }}
+          />
+        )}
+      </View>
     </View>
   );
 }
